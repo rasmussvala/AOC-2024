ad5629a548cbd247a9bf17a30eee72c2c2a29e79
Rasmus Svala <rasmus.svala@hotmail.com>
2024-12-05 16:16:14 +0100

    day 1 part 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        StreamReader reader = new("./data.txt");
+        string? line;
+
+        List<int> left = [];
+        List<int> right = [];
+
+        while ((line = reader.ReadLine()) != null)
+        {
+            string[] numbers = line.Split("   ");
+
+            left.Add(int.Parse(numbers[0]));
+            right.Add(int.Parse(numbers[1]));
+        }
+
+        left.Sort();
+        right.Sort();
+
+        int sum = 0;
+        for (int i = 0; i < left.Count; i++)
+        {
+            sum += Math.Abs(left[i] - right[i]);
+        }
+
+        Console.WriteLine(sum);
+    }
+}
